5b938f154f0916238021ec3e16b0e31c8f3ccae6
Abdullah Elshabrawy <abdullahyasinforwork@gmail.com>
2022-09-10 23:37:03 +0200

    feat: build menu of options to choose desired region

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <select>
+        <option value="selectRegion">select Region</option>
+        <option value="africa">Africa</option>
+        <option value="americas">Americas</option>
+        <option value="asia">Asia</option>
+        <option value="europe">Europe</option>
+        <option value="oceania">Oceania</option>
+      </select>
+    </div>
+  );
 }
 
 export default App;
