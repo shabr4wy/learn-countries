1faa060cfd32cd5e475ef884ed62269663bf1e6f
Abdullah Elshabrawy <abdullahyasinforwork@gmail.com>
2022-09-11 22:30:26 +0200

    feat: improve a11y by using semantic html

--- src/App.js
+++ src/App.js
@@ -30,43 +30,48 @@ function App() {
 
   return (
     <div className="App">
-      <select
-        value={selectedRegion ? selectedRegion : "selectRegion"}
-        onChange={(e) => handleChange(e.target.value)}
-      >
-        <option value="selectRegion">select Region</option>
-        <option value="africa">Africa</option>
-        <option value="americas">Americas</option>
-        <option value="asia">Asia</option>
-        <option value="europe">Europe</option>
-        <option value="oceania">Oceania</option>
-      </select>
+      {/* region menu */}
+      <section className="regionMenu">
+        <select
+          value={selectedRegion ? selectedRegion : "selectRegion"}
+          onChange={(e) => handleChange(e.target.value)}
+        >
+          <option value="selectRegion">select Region</option>
+          <option value="africa">Africa</option>
+          <option value="americas">Americas</option>
+          <option value="asia">Asia</option>
+          <option value="europe">Europe</option>
+          <option value="oceania">Oceania</option>
+        </select>
+      </section>
 
       {/* countries list */}
-      <ul className="countriesList">
-        {countriesArray &&
-          countriesArray.map((country) => (
-            <li key={country.name.common} className="countryItem">
-              <div className="countryFlag">
-                <img
-                  src={country.flags.svg}
-                  height="60"
-                  width="100"
-                  loading="lazy"
-                ></img>
-              </div>
+      <section>
+        <ul className="countriesList">
+          {countriesArray &&
+            countriesArray.map((country) => (
+              <li key={country.name.common} className="countryItem">
+                <div className="countryFlag">
+                  <img
+                    src={country.flags.svg}
+                    height="60"
+                    width="100"
+                    loading="lazy"
+                  ></img>
+                </div>
 
-              <div className="countryInfo">
-                <p className="countryName">{country.name.common}</p>
-                <p className="countryCapital">Capital: {country.capital}</p>
-                <p className="countryRegion">Region: {country.region}</p>
-                <p className="countryPopulation">
-                  Population: {country.population.toLocaleString("en-US")}
-                </p>
-              </div>
-            </li>
-          ))}
-      </ul>
+                <div className="countryInfo">
+                  <p className="countryName">{country.name.common}</p>
+                  <p className="countryCapital">Capital: {country.capital}</p>
+                  <p className="countryRegion">Region: {country.region}</p>
+                  <p className="countryPopulation">
+                    Population: {country.population.toLocaleString("en-US")}
+                  </p>
+                </div>
+              </li>
+            ))}
+        </ul>
+      </section>
     </div>
   );
 }
