6a58ff3515fa48b7b92ceaa11ad6dd64b9074942
Abdullah Elshabrawy <abdullahyasinforwork@gmail.com>
2022-09-11 23:23:06 +0200

    feat: make search country a controlled component

--- src/SearchCountry.js
+++ src/SearchCountry.js
@@ -1,7 +1,16 @@
+import { useState } from "react";
+
 export function SearchCountry({ countriesArray }) {
+  const [searchedCountry, setSearchedCountry] = useState("");
+
+  function handleChange(value) {
+    setSearchedCountry(value);
+  }
   return (
     <section className="search">
       <input
+        value={searchedCountry && searchedCountry}
+        onChange={(e) => handleChange(e.target.value)}
         className="search__input"
         placeholder="search for a country..."
       ></input>
