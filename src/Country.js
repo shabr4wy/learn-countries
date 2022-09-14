36f771759bb9da814c64a870240160716e7d9505
Abdullah Elshabrawy <abdullahyasinforwork@gmail.com>
2022-09-14 03:31:59 +0200

    feat: fetch country data when clicked On
    if user clicked a country from the search page, the browser will open
    a new page to show furthur data about that country.

--- src/Country.js
+++ src/Country.js
@@ -1,9 +1,26 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 export function Country() {
+  let params = useParams();
+  const [countryData, setCountryData] = useState("");
+
+  useEffect(() => {
+    async function getCountry() {
+      await fetch(`https://restcountries.com/v3.1/name/${params.countryName}`)
+        .then((res) => {
+          return res.json();
+        })
+        .then((searchResult) => {
+          setCountryData({ ...searchResult[0] });
+        });
+    }
+    getCountry();
+  }, [params]);
+
   return (
     <div>
-      country Detail
       <Link to="/">back</Link>
     </div>
   );
