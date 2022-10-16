93997c48d1bd4070991dd1beb70380fb13a7c80b
Abdullah Elshabrawy <abdullahyasinforwork@gmail.com>
2022-10-16 11:50:00 +0200

    fix: fix state access

--- src/components/CountriesList.js
+++ src/components/CountriesList.js
@@ -5,49 +5,49 @@ export function CountriesList({
   isCountryFounded,
   toggleElementBackground,
 }) {
+  let dataToRender = countries[0]?.data;
   return (
     // countries list
     <section>
-      {isCountryFounded ? (
+      {countries && dataToRender?.message !== "Not Found" ? (
         <ul className="countriesList">
-          {countries &&
-            countries.map((country) => (
-              <li
-                key={country.name.common}
-                className="countryItem"
-                style={{ background: toggleElementBackground() }}
-              >
-                <Link to={`/learn-countries/${country.cca2}`}>
-                  <div className="countriesList__countryFlag">
-                    <img
-                      src={country.flags.svg}
-                      alt={country.name.common + " flag"}
-                      loading="lazy"
-                    ></img>
-                  </div>
+          {dataToRender?.map((country) => (
+            <li
+              key={country.name.common}
+              className="countryItem"
+              style={{ background: toggleElementBackground() }}
+            >
+              <Link to={`/learn-countries/${country.cca2}`}>
+                <div className="countriesList__countryFlag">
+                  <img
+                    src={country.flags.svg}
+                    alt={country.name.common + " flag"}
+                    loading="lazy"
+                  ></img>
+                </div>
 
-                  <ul className="countriesList__countryGeogrpahy">
-                    <li>
-                      <strong> {country.name.common} </strong>{" "}
-                    </li>
-                    <li>
-                      {" "}
-                      <span className="countryDataLabel">Capital: </span>{" "}
-                      {country.capital}
-                    </li>
-                    <li>
-                      {" "}
-                      <span className="countryDataLabel">Region: </span>{" "}
-                      {country.region}
-                    </li>
-                    <li>
-                      <span className="countryDataLabel">Population: </span>{" "}
-                      {country.population.toLocaleString("en-US")}
-                    </li>
-                  </ul>
-                </Link>
-              </li>
-            ))}
+                <ul className="countriesList__countryGeogrpahy">
+                  <li>
+                    <strong> {country.name.common} </strong>{" "}
+                  </li>
+                  <li>
+                    {" "}
+                    <span className="countryDataLabel">Capital: </span>{" "}
+                    {country.capital}
+                  </li>
+                  <li>
+                    {" "}
+                    <span className="countryDataLabel">Region: </span>{" "}
+                    {country.region}
+                  </li>
+                  <li>
+                    <span className="countryDataLabel">Population: </span>{" "}
+                    {country.population.toLocaleString("en-US")}
+                  </li>
+                </ul>
+              </Link>
+            </li>
+          ))}
         </ul>
       ) : (
         <p className="noSearchResult">
