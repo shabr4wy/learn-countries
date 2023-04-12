0b2c6452283a32542714e17581d837e99291bcdf
Abdullah Elshabrawy <abdullahyasinforwork@gmail.com>
2023-04-12 22:27:45 +0200

    feat: update border countries component

--- src/components/BorderCountries.js
+++ src/components/BorderCountries.js
@@ -1,41 +1,23 @@
-import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+/** @format */
 
-export function BorderCountries({ countryData }) {
-  const [borderCountries, setBorderCountries] = useState("");
+import { Link } from "next/link";
 
-  useEffect(() => {
-    async function getBorders() {
-      countryData &&
-        countryData.borders &&
-        (await fetch(
-          `https://restcountries.com/v3.1/alpha?codes=${countryData.borders.join()}`
-        )
-          .then((res) => {
-            return res.json();
-          })
-          .then((borderCountries) => {
-            setBorderCountries([...borderCountries]);
-          }));
-    }
-    getBorders();
-  }, [countryData]);
-
+export function BorderCountries({ borderCountriesData }) {
   return (
     <div className="countryPage__borders">
       <p className="countryPage__bordersLabel">
         Border Countries:
-        {!borderCountries && (
+        {!borderCountriesData && (
           <span className="countryPage__noBorders"> No border countries</span>
         )}
       </p>
 
-      {borderCountries && (
+      {borderCountriesData && (
         <ul className="countryPage_bordersList">
-          {borderCountries &&
-            borderCountries.map((borderCountry) => (
+          {borderCountriesData &&
+            borderCountriesData.map((borderCountry) => (
               <li key={borderCountry.name.common}>
-                <Link to={`/learn-countries/${borderCountry.cca2}`}>
+                <Link href={`country/${borderCountry.cca2}`}>
                   {borderCountry.name.common}
                 </Link>
               </li>
