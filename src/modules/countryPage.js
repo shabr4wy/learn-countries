bbdb8d13703273c25beae27eb78e3e591b3a7eac
Abdullah Elshabrawy <abdullahyasinforwork@gmail.com>
2023-04-12 21:32:30 +0200

    feat: get countries code to fetch data by code

--- src/modules/countryPage.js
+++ src/modules/countryPage.js
@@ -7,3 +7,23 @@ export async function getCountryData(context) {
 
   return await countryData.json();
 }
+
+export async function storeCountriesCode(allCountriesData) {
+  let countriesCode = [];
+
+  allCountriesData?.map((country) => {
+    countriesCode.push(`${country.cca2}`);
+  });
+
+  return countriesCode;
+}
+
+// country code is used to fetch country data
+export async function getCountreisCodes() {
+  const res = await fetch(`https://restcountries.com/v3.1/all`);
+  const allCountriesData = await res.json();
+
+  const countriesCode = await storeCountriesCode(allCountriesData);
+
+  return countriesCode;
+}
