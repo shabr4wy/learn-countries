5a3039fe43011bbd87b29cb575e2179ea494b0b8
Abdullah Elshabrawy <abdullahyasinforwork@gmail.com>
2023-04-12 21:57:16 +0200

    feat: get paths of all countries

--- src/pages/country/[countryCode].js
+++ src/pages/country/[countryCode].js
@@ -1,7 +1,26 @@
 /** @format */
 
-const CountryPage = () => {
-  return <Country />;
+import { Country } from "../../components/Country";
+import { getCountreisCodes } from "../../modules/countryPage";
+
+export async function getStaticPaths() {
+  // code is used to fetch country data
+  const countriesCode = await getCountreisCodes();
+
+  const paths = countriesCode.map((countryCode) => {
+    return { params: { countryCode } };
+  });
+
+  return { paths, fallback: false };
+}
+
+const CountryPage = ({ countryData, borderCountriesData }) => {
+  return (
+    <Country
+      countryData={countryData[0]}
+      borderCountriesData={borderCountriesData}
+    />
+  );
 };
 
 export default CountryPage;
