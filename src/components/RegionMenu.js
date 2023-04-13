46db5ffbd6602d9730f3ec4b21d7f25af0c29429
Abdullah Elshabrawy <abdullahyasinforwork@gmail.com>
2023-04-13 22:55:01 +0200

    refactor: refactor region menu component
    problem: select/option elements doesn't allow Link/anchor component to be nested inside.
    
    solution: I used a ul element instead of select

--- src/components/RegionMenu.js
+++ src/components/RegionMenu.js
@@ -1,31 +1,27 @@
-export function RegionMenu({ setSelectedRegion, selectedRegion }) {
-  const handleChange = (value) => {
-    setSelectedRegion(value);
-  };
+/** @format */
 
+import Link from "next/link";
+
+export function RegionMenu() {
   return (
-    //region menu
     <section className="regionMenu">
-      <select
-        value={selectedRegion}
-        onChange={(e) => handleChange(e.target.value)}
-      >
-        <option value="selectRegion">select Region</option>
-        <option value="africa">Africa</option>
-        <option value="americas">Americas</option>
-        <option value="asia">Asia</option>
-        <option value="europe">Europe</option>
-        <option value="oceania">Oceania</option>
-      </select>
-
-      <svg
-        className="regionMenu__icon"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 320 512"
-      >
-        {/* Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. */}
-        <path d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z" />
-      </svg>
+      <ul className="regionMenu__list">
+        <li>
+          <Link href="countries/africa">Africa</Link>
+        </li>
+        <li>
+          <Link href="countries/americas">Americas</Link>
+        </li>
+        <li>
+          <Link href="countries/asia">Asia</Link>
+        </li>
+        <li>
+          <Link href="countries/europe">Europe</Link>
+        </li>
+        <li>
+          <Link href="countries/oceania">Oceania</Link>
+        </li>
+      </ul>
     </section>
   );
 }
