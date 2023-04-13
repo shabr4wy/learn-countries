352c8f66faeee3da78162d622d1aa63ac362e902
Abdullah Elshabrawy <abdullahyasinforwork@gmail.com>
2023-04-13 23:02:59 +0200

    fix: fix paths

--- src/components/RegionMenu.js
+++ src/components/RegionMenu.js
@@ -7,19 +7,19 @@ export function RegionMenu() {
     <section className="regionMenu">
       <ul className="regionMenu__list">
         <li>
-          <Link href="countries/africa">Africa</Link>
+          <Link href="/countries/africa">Africa</Link>
         </li>
         <li>
-          <Link href="countries/americas">Americas</Link>
+          <Link href="/countries/americas">Americas</Link>
         </li>
         <li>
-          <Link href="countries/asia">Asia</Link>
+          <Link href="/countries/asia">Asia</Link>
         </li>
         <li>
-          <Link href="countries/europe">Europe</Link>
+          <Link href="/countries/europe">Europe</Link>
         </li>
         <li>
-          <Link href="countries/oceania">Oceania</Link>
+          <Link href="/countries/oceania">Oceania</Link>
         </li>
       </ul>
     </section>
