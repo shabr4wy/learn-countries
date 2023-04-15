8c9c33842e4b6c144bac9fa4e66797cbfeb8b617
Abdullah Elshabrawy <abdullahyasinforwork@gmail.com>
2023-04-15 21:46:35 +0200

    feat: remove unecessary code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,75 +1,14 @@
 /** @format */
-
-// /** @format */
-
-import { useState, useEffect } from "react";
 import { RegionMenu } from "../components/RegionMenu";
 import { SearchCountry } from "../components/SearchCountry";
 import { Header } from "../components/Header";
 
 function index() {
-  const [selectedRegion, setSelectedRegion] = useState("");
-  const [searchedCountry, setSearchedCountry] = useState("");
-
-  // fetch countries data
-  useEffect(() => {
-    const controller = new AbortController();
-    const { signal } = controller;
-
-    const getCountries = async () => {
-      if (selectedRegion === "selectRegion") {
-        deleteSearchResult("input");
-      } else if (selectedRegion) {
-        await fetch(`https://restcountries.com/v3.1/region/${selectedRegion}`, {
-          signal,
-        })
-          .then((res) => {
-            return res.json();
-          })
-          .then((regionCountries) => {
-            updateSearchResult("select", regionCountries);
-          });
-      }
-    };
-    getCountries();
-
-    // clean up effect
-    return () => controller.abort();
-  }, [selectedRegion]);
-
-  // get input result
-  useEffect(() => {
-    const controller = new AbortController();
-    const { signal } = controller;
-
-    if (searchedCountry) {
-      async function getCountry() {
-        await fetch(`https://restcountries.com/v3.1/name/${searchedCountry}`, {
-          signal,
-        })
-          .then((res) => {
-            return res.json();
-          })
-          .then((searchResult) => {})
-          .catch(() => {
-            !signal.aborted;
-          });
-      }
-      getCountry();
-    }
-
-    // clean up effect
-    return () => controller.abort();
-  }, [searchedCountry]);
-
   return (
     <>
       <Header />
       <main className="countriesSearchPage main">
-        <SearchCountry
-          setSearchedCountry={setSearchedCountry}
-          searchedCountry={searchedCountry}
-        />
+        <SearchCountry />
         <RegionMenu />
       </main>
     </>
