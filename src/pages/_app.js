a8c6fc9ff9b20325f344c4897c7ef78e52a24a6e
Abdullah Elshabrawy <abdullahyasinforwork@gmail.com>
2023-04-15 22:48:19 +0200

    feat: create searched country context

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,17 @@
 /** @format */
+import { createContext, useState } from "react";
 import "../styles/sass/App.scss";
 
+export const searchedCountryContext = createContext("");
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [searchedCountry, setSearchedCountry] = useState("");
+
+  return (
+    <searchedCountryContext.Provider
+      value={{ searchedCountry, setSearchedCountry }}
+    >
+      <Component {...pageProps} />
+    </searchedCountryContext.Provider>
+  );
 }
