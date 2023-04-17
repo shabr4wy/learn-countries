956d601b18803944003ac6c155ee6c8903f26de1
Abdullah Elshabrawy <abdullahyasinforwork@gmail.com>
2023-04-17 02:32:28 +0200

    feat: fetch searched countries data

--- src/pages/SearchResult.js
+++ src/pages/SearchResult.js
@@ -6,17 +6,28 @@ import { Header } from "../components/Header";
 import { RegionMenu } from "../components/RegionMenu";
 import { SearchCountry } from "../components/SearchCountry";
 import { searchedCountryContext } from "./_app";
+import useSWR from "swr";
 
 const SearchResult = () => {
   const { searchedCountry } = useContext(searchedCountryContext);
-  console.log(searchedCountry);
+
+  const getSearchResult = async () => {
+    const res = await fetch(
+      `https://restcountries.com/v3.1/name/${searchedCountry}?fields=name,flags,population,region,capital,cca2`
+    );
+    const data = await res.json();
+
+    return data;
+  };
+
+  const { data } = useSWR(searchedCountry && "searchResult", getSearchResult);
   return (
     <>
       <Header />
       <main className="countriesSearchPage main">
         <SearchCountry />
         <RegionMenu />
-        <CountriesList />
+        <CountriesList countries={data} />
       </main>
     </>
   );
