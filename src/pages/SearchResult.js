9fac5ddf26870953ab64ddbe1bd7ba65e055a328
Abdullah Elshabrawy <abdullahyasinforwork@gmail.com>
2023-04-17 02:40:47 +0200

    feat: fetch data by param to share url

--- src/pages/SearchResult.js
+++ src/pages/SearchResult.js
@@ -1,14 +1,15 @@
 /** @format */
 
-import { useContext } from "react";
 import { CountriesList } from "../components/CountriesList";
 import { Header } from "../components/Header";
-import { searchedCountryContext } from "./_app";
 import useSWR from "swr";
+import { useRouter } from "next/router";
 
 const SearchResult = () => {
-  const { searchedCountry } = useContext(searchedCountryContext);
+  const router = useRouter();
+  const searchedCountry = router.query.search;
 
+  console.log(searchedCountry);
   const getSearchResult = async () => {
     const res = await fetch(
       `https://restcountries.com/v3.1/name/${searchedCountry}?fields=name,flags,population,region,capital,cca2`
