69dcc7ab8c19cccf0553189f53667909d54405ca
Abdullah Elshabrawy <abdullahyasinforwork@gmail.com>
2023-04-18 20:44:11 +0200

    feat: enhance UX

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
+/** @format */
+
+import Link from "next/link.js";
 import { SwitchTheme } from "./SwitchTheme.js";
 
 export function Header() {
   return (
     <header className="header">
       <div className="header__title">
-        <h2>Search The World</h2>
+        <Link href="/">
+          <h2>Search The World</h2>
+        </Link>
       </div>
 
       <SwitchTheme />
