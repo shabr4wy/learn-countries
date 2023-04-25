499ab9dd2f5eb9bfb47d46b2b380bd779d4d6d11
Abdullah Elshabrawy <abdullahyasinforwork@gmail.com>
2023-04-25 04:38:10 +0200

    feat: improve accessibility

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@
 import { createContext, useState } from "react";
 import "../styles/App.css";
 import { Analytics } from "@vercel/analytics/react";
+import Head from "next/head";
 
 export const searchedCountryContext = createContext("");
 
@@ -12,6 +13,15 @@ export default function App({ Component, pageProps }) {
     <searchedCountryContext.Provider
       value={{ searchedCountry, setSearchedCountry }}
     >
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="author" content="Abdullah Elshabrawy" />
+        <meta
+          name="description"
+          content="a website that shows countries' data such as flag, city capital, population number, region, border countries, and much more"
+        />
+        <title>Learn Countries</title>
+      </Head>
       <Component {...pageProps} />
 
       <Analytics />
