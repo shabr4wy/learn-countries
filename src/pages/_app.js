c109662b32211f42d0c64ba8fc835f5c5881cc11
Abdullah Elshabrawy <abdullahyasinforwork@gmail.com>
2023-05-04 21:13:40 +0300

    feat: show loader when loading pages

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,11 +3,18 @@ import { createContext, useState } from "react";
 import "../styles/App.css";
 import { Analytics } from "@vercel/analytics/react";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { Loader } from "../components/Loader";
 
 export const searchedCountryContext = createContext("");
 
 export default function App({ Component, pageProps }) {
   const [searchedCountry, setSearchedCountry] = useState("");
+
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+  router.events?.on("routeChangeStart", () => setIsLoading(true));
+  router.events?.on("routeChangeComplete", () => setIsLoading(false));
 
   return (
     <searchedCountryContext.Provider
@@ -23,6 +30,7 @@ export default function App({ Component, pageProps }) {
         <title>Learn Countries</title>
       </Head>
       <Component {...pageProps} />
+      {isLoading && <Loader />}
 
       <Analytics />
     </searchedCountryContext.Provider>
